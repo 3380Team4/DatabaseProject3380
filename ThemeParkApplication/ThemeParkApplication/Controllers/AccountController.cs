1495ddbce83488825eea24e25b45fb65eb2bfcab
giangha <gi4ngh495@gmail.com>
2018-04-13 12:44:34 -0500

    add login button

---
+++ ThemeParkApplication/ThemeParkApplication/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThemeParkApplication.Models;
+
+namespace ThemeParkApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            ViewBag.Title = "Login Page";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login vm)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(vm);
+        }
+    }
+}
