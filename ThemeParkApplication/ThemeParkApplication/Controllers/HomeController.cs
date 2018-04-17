f950a494a15d1cab4adf4c8aad71af667b148651
giangha <gi4ngh495@gmail.com>
2018-04-16 19:17:24 -0500

    change layout

--- ThemeParkApplication/ThemeParkApplication/Controllers/HomeController.cs
+++ ThemeParkApplication/ThemeParkApplication/Controllers/HomeController.cs
@@ -12,20 +12,16 @@ namespace ThemeParkApplication.Controllers
     {
         public IActionResult Index()
         {
-            return Redirect("Account/Login");
+            return View();
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
