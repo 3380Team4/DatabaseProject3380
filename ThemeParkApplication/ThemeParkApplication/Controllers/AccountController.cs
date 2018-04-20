7df9ce0f8d0fc1fa5a1a08e93d4c4145cc53bbc3
Navin <ghimire.navin@gmail.com>
2018-04-19 19:34:30 -0500

    Additional fixes to the Layout including navbar

--- ThemeParkApplication/ThemeParkApplication/Controllers/AccountController.cs
+++ ThemeParkApplication/ThemeParkApplication/Controllers/AccountController.cs
@@ -64,7 +64,7 @@ namespace ThemeParkApplication.Controllers
                 var result = await _signInManager.PasswordSignInAsync(vm.Email, vm.Password, vm.RememberMe, false);
                 if( result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Home/About");
+                    return RedirectToAction("Index", "Home");
                 }
                 ModelState.AddModelError("", "Invalid Login Attempt.");
                 return View(vm);
