e38340b57ea976b2fd591a6263c2c7a9b590017b
giangha <gi4ngh495@gmail.com>
2018-04-23 02:24:05 -0500

    add weekly customer graph

--- ThemeParkApplication/ThemeParkApplication/Controllers/HomeController.cs
+++ ThemeParkApplication/ThemeParkApplication/Controllers/HomeController.cs
@@ -19,65 +19,109 @@ namespace ThemeParkApplication.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var dataPoints1 = new List<DataPoint>();
-            int[] CustomerNum = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            
+            var dataWeekly = new List<DataPoint>();
+            int[] weeklyCustomer = new int[] { 0, 0, 0, 0, 0, 0, 0 };
             var themeparkdbContext = _context.Transactions;
 
             foreach (Transactions t in themeparkdbContext)
             {
-                if(t.DateOfSale.Year == 2018)
+                for (var Day = DateTime.Now.Day - 7; Day < DateTime.Now.Day; Day++)
                 {
-                    Debug.Write("YEAHHHHH");
-                    if (t.DateOfSale.Month == 1)
+                    if (t.DateOfSale.Day == Day)
                     {
-                        CustomerNum[0]++;
-                        Debug.Write(CustomerNum[0]);
+                        if (t.DateOfSale.DayOfWeek == DayOfWeek.Monday)
+                            weeklyCustomer[0]++;
+                        else if (t.DateOfSale.DayOfWeek == DayOfWeek.Tuesday)
+                            weeklyCustomer[1]++;
+                        else if (t.DateOfSale.DayOfWeek == DayOfWeek.Wednesday)
+                            weeklyCustomer[2]++;
+                        else if (t.DateOfSale.DayOfWeek == DayOfWeek.Thursday)
+                            weeklyCustomer[3]++;
+                        else if (t.DateOfSale.DayOfWeek == DayOfWeek.Friday)
+                            weeklyCustomer[4]++;
+                        else if (t.DateOfSale.DayOfWeek == DayOfWeek.Saturday)
+                            weeklyCustomer[5]++;
+                        else if (t.DateOfSale.DayOfWeek == DayOfWeek.Sunday)
+                            weeklyCustomer[6]++;
                     }
-                    else if (t.DateOfSale.Month == 2)
-                        CustomerNum[1]++;
-                    else if (t.DateOfSale.Month == 3)
-                        CustomerNum[2]++;
-                    else if (t.DateOfSale.Month == 4)
-                        CustomerNum[3]++;
                 }
-                else if(t.DateOfSale.Year == 2017)
+
+            }
+
+            dataWeekly.Add(new DataPoint("Mon", weeklyCustomer[0]));
+            dataWeekly.Add(new DataPoint("Tue", weeklyCustomer[1]));
+            dataWeekly.Add(new DataPoint("Wed", weeklyCustomer[2]));
+            dataWeekly.Add(new DataPoint("thu", weeklyCustomer[3]));
+            dataWeekly.Add(new DataPoint("Fri", weeklyCustomer[4]));
+            dataWeekly.Add(new DataPoint("Sat", weeklyCustomer[5]));
+            dataWeekly.Add(new DataPoint("Sun", weeklyCustomer[6]));
+
+           
+            ViewBag.dataWeekly = JsonConvert.SerializeObject(dataWeekly);
+
+            return View();
+        }
+        
+        public IActionResult MonthlyCustomer()
+        {
+            var dataMonthly = new List<DataPoint>();
+            int[] monthlyCustomer = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            var themeparkdbContext = _context.Transactions;
+
+            foreach (Transactions t in themeparkdbContext)
+            {
+                if (t.DateOfSale.Year == 2018)
+                {
+                    if (t.DateOfSale.Month == 1)
+                        monthlyCustomer[0]++;
+                    else if (t.DateOfSale.Month == 2)
+                        monthlyCustomer[1]++;
+                    else if (t.DateOfSale.Month == 3)
+                        monthlyCustomer[2]++;
+                    else if (t.DateOfSale.Month == 4)
+                    {
+                        monthlyCustomer[3]++;
+                        
+                    }
+                }
+                else if (t.DateOfSale.Year == 2017)
                 {
                     if (t.DateOfSale.Month == 12)
-                        CustomerNum[11]++;
+                        monthlyCustomer[11]++;
                     else if (t.DateOfSale.Month == 11)
-                        CustomerNum[10]++;
+                        monthlyCustomer[10]++;
                     else if (t.DateOfSale.Month == 10)
-                        CustomerNum[9]++;
+                        monthlyCustomer[9]++;
                     else if (t.DateOfSale.Month == 9)
-                        CustomerNum[8]++;
+                        monthlyCustomer[8]++;
                     else if (t.DateOfSale.Month == 8)
-                        CustomerNum[7]++;
+                        monthlyCustomer[7]++;
                     else if (t.DateOfSale.Month == 7)
-                        CustomerNum[6]++;
+                        monthlyCustomer[6]++;
                     else if (t.DateOfSale.Month == 6)
-                        CustomerNum[5]++;
+                        monthlyCustomer[5]++;
                     else if (t.DateOfSale.Month == 5)
-                        CustomerNum[4]++;
+                        monthlyCustomer[4]++;
                 }
             }
 
 
+            dataMonthly.Add(new DataPoint("Jan", monthlyCustomer[0]));
+            dataMonthly.Add(new DataPoint("Feb", monthlyCustomer[1]));
+            dataMonthly.Add(new DataPoint("Mar", monthlyCustomer[2]));
+            dataMonthly.Add(new DataPoint("Apr", monthlyCustomer[3]));
+            dataMonthly.Add(new DataPoint("May", monthlyCustomer[4]));
+            dataMonthly.Add(new DataPoint("Jun", monthlyCustomer[5]));
+            dataMonthly.Add(new DataPoint("Jul", monthlyCustomer[6]));
+            dataMonthly.Add(new DataPoint("Aug", monthlyCustomer[7]));
+            dataMonthly.Add(new DataPoint("Sep", monthlyCustomer[8]));
+            dataMonthly.Add(new DataPoint("Oct", monthlyCustomer[9]));
+            dataMonthly.Add(new DataPoint("Nov", monthlyCustomer[10]));
+            dataMonthly.Add(new DataPoint("Dec", monthlyCustomer[11]));
 
-            dataPoints1.Add(new DataPoint("Jan", CustomerNum[0]));
-            dataPoints1.Add(new DataPoint("Feb", CustomerNum[1]));
-            dataPoints1.Add(new DataPoint("Mar", CustomerNum[2]));
-            dataPoints1.Add(new DataPoint("Apr", CustomerNum[3]));
-            dataPoints1.Add(new DataPoint("May", CustomerNum[4]));
-            dataPoints1.Add(new DataPoint("Jun", CustomerNum[5]));
-            dataPoints1.Add(new DataPoint("Jul", CustomerNum[6]));
-            dataPoints1.Add(new DataPoint("Aug", CustomerNum[7]));
-            dataPoints1.Add(new DataPoint("Sep", CustomerNum[8]));
-            dataPoints1.Add(new DataPoint("Oct", CustomerNum[9]));
-            dataPoints1.Add(new DataPoint("Nov", CustomerNum[10]));
-            dataPoints1.Add(new DataPoint("Dec", CustomerNum[11]));
-
-            ViewBag.DataPoints1 = JsonConvert.SerializeObject(dataPoints1);
-
+            ViewBag.dataMonthly = JsonConvert.SerializeObject(dataMonthly);
             return View();
         }
 
